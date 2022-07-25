938d686cf7f9c4a669abd0388b823bb2c0d91210
Stuart-Yee <ki6fzb@gmail.com>
2022-07-24 18:13:40 -0700

    altered object to have a liked_by set and query results are filtered

--- flask_app/models/woodproject.py
+++ flask_app/models/woodproject.py
@@ -1,5 +1,6 @@
 from multiprocessing import allow_connection_pickling
 from operator import truediv
+from unittest import result
 from flask_app import app
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
@@ -22,7 +23,8 @@ class Woodproject:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         self.user_id = data['user_id']
-        self.user = user.User.get_one_by_id({'id':self.user_id}) #This pulls in the user who created each work project
+        self.liked_by = set() # for number of likes, can just use len({instance}.like_by)
+        self.user = None
 
 
 ###################################### 
@@ -47,6 +49,46 @@ class Woodproject:
         for row in results:
             all_woodprojects.append(cls(row))
         return all_woodprojects
+
+    @classmethod
+    def get_all_with_likes(cls):
+        # Standard left join because group by won't work on 
+        # specific MYSQL server
+        query = "SELECT * FROM woodprojects " \
+            "LEFT JOIN favorites on woodprojects.id = "\
+            "favorites.woodproject_id; "
+
+        results = connectToMySQL(cls.db).query_db(query)
+        if len(results) < 1:
+            return None
+        project_ids = set()
+        projects = []
+        for i in range(len(results)):
+            this_id = results[i]["id"]
+            user_id = results[i]["favorites.user_id"]
+            if this_id not in project_ids:    # only create an object if not already done
+                project_ids.add(this_id)   # keep track of the projects we've found
+                project = cls(results[i])
+                if user_id: # check if this exists
+                    project.liked_by.add(user_id) # add the id of the user that liked it
+                projects.append(project)
+            else: # we've already found this project previously
+                if projects[-1].id == this_id: # this row likely references the same project in the last row
+                    ref_idx = len(projects) - 1
+                else:
+                    for x in range(len(projects)):  # brute force search otherwise
+                        if projects[x].id == this_id:
+                            ref_idx = x
+                            break
+                if user_id:
+                    projects[ref_idx].liked_by.add(user_id) # we don't want add a new project, but we do want to add
+                                                        # to its liked by
+        return projects
+
+
+
+
+
 
     @classmethod
     def get_all_woodprojects_by_one_user(cls,data):
